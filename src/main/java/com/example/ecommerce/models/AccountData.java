f734a9871f8bd69dbbd4cd1913f0b5639b94ac73
gaurav kumar <gauravkumar123@gmail.com>
2021-10-03 22:35:42 +0530

    lambok dependency added

--- src/main/java/com/example/ecommerce/models/AccountData.java
+++ src/main/java/com/example/ecommerce/models/AccountData.java
@@ -1,42 +1,18 @@
 package com.example.ecommerce.models;
 
+import lombok.*;
+
 import java.util.List;
 
+
+@Builder
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class AccountData {
     private Customer customer;
     private List<Address> addresses;
     private List<CartItem> cartItems;
     private List<Orders> ordersList;
-
-    public Customer getCustomer() {
-        return customer;
-    }
-
-    public void setCustomer(Customer customer) {
-        this.customer = customer;
-    }
-
-    public List<Address> getAddresses() {
-        return addresses;
-    }
-
-    public void setAddresses(List<Address> addresses) {
-        this.addresses = addresses;
-    }
-
-    public List<CartItem> getCartItems() {
-        return cartItems;
-    }
-
-    public void setCartItems(List<CartItem> cartItems) {
-        this.cartItems = cartItems;
-    }
-
-    public List<Orders> getOrdersList() {
-        return ordersList;
-    }
-
-    public void setOrdersList(List<Orders> ordersList) {
-        this.ordersList = ordersList;
-    }
 }
